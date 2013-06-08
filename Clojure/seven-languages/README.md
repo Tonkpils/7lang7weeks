7f5ab324040a41fb9094ac58d399f3dd689e60d6
Leo Correa <lcorr005@gmail.com>
2013-06-08 17:12:52 -0400

    Closure template project with lein

---
+++ Clojure/seven-languages/README.md
@@ -0,0 +1,13 @@
+# seven-languages
+
+Clojure Chapter
+
+## Usage
+
+Don't use me :)
+
+## License
+
+Copyright © 2013 Leo Correa
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ Clojure/seven-languages/test/seven_languages/core_test.clj
@@ -0,0 +1,7 @@
+(ns seven-languages.core-test
+  (:require [clojure.test :refer :all]
+            [seven-languages.core :refer :all]))
+
+(deftest a-test
+  (testing "1 equals 1"
+    (is (= 1 1))))

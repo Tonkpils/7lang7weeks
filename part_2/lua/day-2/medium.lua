4884f75e1634c5732e2ac47731e84c9b2ea44164
Leo Correa <lcorr005@gmail.com>
2016-09-05 17:18:17 -0400

    [Lua] Day 2 Queue

--- part_2/lua/day-2/medium.lua
+++ part_2/lua/day-2/medium.lua
@@ -23,3 +23,44 @@ actual = a + b
 for k, v in ipairs(expected) do
   assert(actual[k] == v)
 end
+
+local Queue = {first = 0, last = -1}
+
+function Queue:new()
+  local obj = {}
+  setmetatable(obj, self)
+  self.__index = self
+  return obj
+end
+
+function Queue:add(item)
+  local last = self.last + 1
+  self.last = last
+  self[last] = item
+end
+
+function Queue:remove()
+  local first = self.first
+  if first > self.last then return nil end
+  local value = self[first]
+  self[first] = nil
+  self.first = first + 1
+
+  return value
+end
+
+local q = Queue:new()
+
+q:add(1)
+q:add(2)
+q:add(4)
+
+assert(q:remove() == 1)
+assert(q:remove() == 2)
+assert(q:remove() == 4)
+assert(q:remove() == nil)
+
+q:add(5)
+assert(q:remove() == 5)
+assert(q:remove() == nil)
+assert(q:remove() == nil)
